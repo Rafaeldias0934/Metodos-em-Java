796aea6a8fbe63391685214ff3e74fba4781b7e2
Rafael Dias <rafaeldias0934@gmail.com>
2022-07-08 07:42:15 -0300

    commit

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,3 +1,9 @@
 public class Calculadora {
 
+    public static void soma(int numero1, int numero2) {
+
+        int resultado = numero1 + numero2;
+
+        System.out.println("a soma de " + numero1 + " mais " + numero2 + " é igual a: " + resultado );
+    }
 }
