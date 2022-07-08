faf27b0b5419e5891a74f39b26fd6c72bafcac55
Rafael Dias <rafaeldias0934@gmail.com>
2022-07-08 07:50:34 -0300

    progress

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -4,6 +4,26 @@ public class Calculadora {
 
         int resultado = numero1 + numero2;
 
-        System.out.println("a soma de " + numero1 + " mais " + numero2 + " é igual a: " + resultado );
+        System.out.println("a soma de " + numero1 + " mais " + numero2 + " é igual a: " + resultado);
+    }
+    public static void subtracao(int numero1, int numero2) {
+
+        int resultado = numero1 - numero2;
+
+        System.out.println("a subtracao de " + numero1 + " menos " + numero2 + " é igual a: " + resultado);
+    }
+
+    public static void multiplicacao(int numero1, int numero2) {
+
+        int resultado = numero1 * numero2;
+
+        System.out.println("a multiplicacao de " + numero1 + " vezes " + numero2 + " é igual a: " + resultado);
+    }
+
+    public static void divisao(int numero1, int numero2) {
+
+        int resultado = numero1 / numero2;
+
+        System.out.println("a divisao de " + numero1 + " por " + numero2 + " é igual a: " + resultado);
     }
 }
