218ef631d2e0fda83ac73b33d9bbdf41713fe8a4
Rafael Dias <rafaeldias0934@gmail.com>
2022-07-08 17:32:38 -0300

    Exercitando

--- src/Sobrecargas/Quadrilatero.java
+++ src/Sobrecargas/Quadrilatero.java
@@ -1,4 +1,19 @@
 package Sobrecargas;
 
 public class Quadrilatero {
+
+    public static void area(double lado) {
+
+        System.out.println("Área do Quadrado:" + (lado * lado));
+    }
+
+    public static void area(double lado1, double lado2) {
+
+        System.out.println("Área do triângulo: " + (lado1 * lado2));
+    }
+
+    public static void area(double baseMaior, double baseMenor, double altura) {
+
+        System.out.println("Área do trapézio:" + ((baseMaior + baseMenor) * altura) / 2);
+    }
 }
