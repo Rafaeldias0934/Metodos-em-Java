fca7a41de3d4fab8160596287eaf6fe771b1d19f
Rafael Dias <rafaeldias0934@gmail.com>
2022-07-08 08:01:21 -0300

    out calculator

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Exercicios da calculadora");
+        Calculadora.soma(35,41);
+        Calculadora.subtracao(14,4);
+        Calculadora.multiplicacao(200,10);
+        Calculadora.divisao(40,10);
+        System.out.println("Hello world!");
+    }
+}
